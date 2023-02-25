7b9c9ac917615cb7d1242c09f5144df27d29cedf
Pedro Arellano <PArellano25@cmc.edu>
2023-02-25 13:20:02 -0800

    main

--- sorting.py
+++ sorting.py
@@ -3,7 +3,7 @@
 Python provides built-in sort/sorted functions that use timsort internally.
 You cannot use these built-in functions anywhere in this file.
 
-Every function in this file takes a comparator `cmp` as input
+Every function in the file takes a comparator `cmp` as input
 which controls how the elements of the list should be
 compared against each other:
 If cmp(a, b) returns -1, then a < b;
